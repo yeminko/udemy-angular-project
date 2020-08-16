b43afad0b047fef87b221b0d6b7320b2e0d2a9e8
Ye Min Ko <yeminko47@gmail.com>
2020-08-16 22:44:35 +0630

    [completed] assignment 3

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isShow: boolean = true;
+  logs = [];
+
+  toggleButton() {
+    this.isShow = !this.isShow;
+    this.logs.push(new Date().getTime());
+  }
 }
